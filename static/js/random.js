6fdef0ee79a52440f9bbeb6556356f1c70ee418e
vvasuki <vishvas.vasuki@gmail.com>
2019-02-22 22:30:38 -0800

    Content update.

--- static/js/random.js
+++ static/js/random.js
@@ -1,9 +1,6 @@
 function redirectToRandomPage(filterFn) {
-    var allSidebarUrls = [];
-    $("#displayed_sidebar li:not(:has(ul)) a").each(function() {allSidebarUrls.push($(this).attr("href"));})
-
-    var filteredUrls = allSidebarUrls.filter(filterFn);
-    var randomMantraUrl = filteredUrls[Math.floor(Math.random()*filteredUrls.length)];
+    var filteredUrls = Object.getOwnPropertyNames(pageUrlToParams).filter(filterFn);
+    var randomMantraUrl = baseURL + filteredUrls[Math.floor(Math.random()*filteredUrls.length)];
     console.log(randomMantraUrl);
 // alert(randomMantraUrl);
     if (randomMantraUrl) {
