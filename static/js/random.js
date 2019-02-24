944e06bd25e84c9b398c63e8c89cd64a38b8bbb8
vvasuki <vishvas.vasuki@gmail.com>
2019-02-24 14:38:50 -0800

    Content update.

--- static/js/random.js
+++ static/js/random.js
@@ -1,10 +1,32 @@
+function weightedRandom(weights) {
+    var totalWeight = 0,
+        i, random;
+
+    for (i = 0; i < weights.length; i++) {
+        totalWeight += weights[i];
+    }
+
+    random = Math.random() * totalWeight;
+
+    for (i = 0; i < weights.length; i++) {
+        if (random < weights[i]) {
+            return i;
+        }
+
+        random -= weights[i];
+    }
+
+    return -1;
+};
+
+
 function redirectToRandomPage(weightingFn) {
     var urls = Object.getOwnPropertyNames(pageUrlToParams);
     var weights = urls.map(weightingFn);
-    var randomMantraUrl = baseURL + filteredUrls[weightedRandom(weights)];
-    console.log(randomMantraUrl);
-// alert(randomMantraUrl);
-    if (randomMantraUrl) {
-        window.location.replace(randomMantraUrl);
+    var randomUrl = baseURL + urls[weightedRandom(weights)];
+    console.log(randomUrl);
+// alert(randomUrl);
+    if (randomUrl) {
+        window.location.replace(randomUrl);
     }
 }
