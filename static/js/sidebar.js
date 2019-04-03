c2b5113d6e9736950ecef668faa41845e7e9762b
vvasuki <vishvas.vasuki@gmail.com>
2019-04-03 14:30:50 -0700

    Content update.

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -107,13 +107,5 @@ function insertNavItems(navbarId, items) {
     }
 }
 
-// Code to make the "Nav" button, which toggles the sidebar.
-var toggleSidebar = function() {
-    $("#sidebar").toggle();
-    // $("#tg-sb-icon").toggleClass('fa-toggle-on');
-    // $("#tg-sb-icon").toggleClass('fa-toggle-off');
-    $("#tg-sb-icon-content-pane").toggleClass('fa-toggle-on');
-    $("#tg-sb-icon-content-pane").toggleClass('fa-toggle-off');
-};
 $("#tg-sb-link").click(toggleSidebar);
 // $("#hide-sb-link").click(toggleSidebar);
