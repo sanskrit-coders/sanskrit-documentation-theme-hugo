ad26ca78831ff5a196eacf078db631a5a7c0d546
vvasuki <vishvas.vasuki@gmail.com>
2020-07-19 20:37:35 +0530

    CU

--- webpack_src/js/spreadsheets.js
+++ webpack_src/js/spreadsheets.js
@@ -1,59 +1,81 @@
 import Handsontable from "handsontable";
 import 'handsontable/dist/handsontable.full.css';
 import * as sidebar from "./sidebar";
+const json5 = require('json5');
+
+function fillTableFromJsObject(data, sheetEmbedTag, headerStr) {
+    const tableData = [];             // The array to store JSON data.
+    let columnWidths = [];
+    let headers = [];
+    if (headerStr === "") {
+        for (let key in data[0]) {
+            headers.push(key);
+            columnWidths.push(20);
+        }
+    } else {
+        headerStr.split(",")
+    }
+    console.debug(headerStr, headers);
+    $.each(data, function (index, value) {
+        tableData.push(data[index]);
+        for (let key in data[index]) {
+            let value = data[index][key];
+            let columnIndex = Object.keys(data[index]).indexOf(key);
+            columnWidths[columnIndex] = Math.max(columnWidths[columnIndex], Math.min(value.length * 20, 200));
+            // console.debug(value, value.length, columnWidths);
+        }
+    });
+    // console.debug(screen.availWidth);
+    const hoTable = new Handsontable(sheetEmbedTag, {
+        data: tableData,
+        rowHeaders: true,
+        colHeaders: headers,
+        colWidths: columnWidths,
+        stretchH: 'all',
+        height: .8*screen.availHeight,
+        // rowHeights: '100px',
+        // preventOverflow: false,
+        manualColumnResize: true,
+        manualRowResize: true,
+        contextMenu: true,
+        autoWrapRow: false,
+        autoWrapCol: false,
+        // fixedRowsTop: 1,
+        minSpareRows: 1,
+        columnSorting: true,
+        bindRowsWithHeaders: true,
+        manualColumnFreeze: true,
+        licenseKey: 'non-commercial-and-evaluation'
+    });
+    sidebar.setupSidebarToggle();
+}
 
 function fillJsonTable(sheetEmbedTag) {
     let jsonUrl = sheetEmbedTag.getAttribute("src");
     let headerStr = sheetEmbedTag.getAttribute("headers") || "";
     $.getJSON(jsonUrl, function (data) {
         if (data.length > 0) {
-            const tableData = [];             // The array to store JSON data.
-            let columnWidths = [];
-            let headers = [];
-            if (headerStr === "") {
-                for (let key in data[0]) {
-                    headers.push(key);
-                    columnWidths.push(20);
-                }
-            } else {
-                headerStr.split(",")
-            }
-            console.debug(headerStr, headers);
-            $.each(data, function (index, value) {
-                tableData.push(data[index]);
-                for (let key in data[index]) {
-                    let value = data[index][key];
-                    let columnIndex = Object.keys(data[index]).indexOf(key);
-                    columnWidths[columnIndex] = Math.max(columnWidths[columnIndex], Math.min(value.length * 20, 200));
-                    // console.debug(value, value.length, columnWidths);
-                }
-            });
-            // console.debug(screen.availWidth);
-            const hoTable = new Handsontable(sheetEmbedTag, {
-                data: tableData,
-                rowHeaders: true,
-                colHeaders: headers,
-                colWidths: columnWidths,
-                stretchH: 'all',
-                height: .8*screen.availHeight,
-                // rowHeights: '100px',
-                // preventOverflow: false,
-                manualColumnResize: true,
-                manualRowResize: true,
-                contextMenu: true,
-                autoWrapRow: false,
-                autoWrapCol: false,
-                // fixedRowsTop: 1,
-                minSpareRows: 1,
-                columnSorting: true,
-                bindRowsWithHeaders: true,
-                manualColumnFreeze: true,
-                licenseKey: 'non-commercial-and-evaluation'
-            });
-            sidebar.setupSidebarToggle();
+            fillTableFromJsObject(data, sheetEmbedTag, headerStr);
         }
     });
+}
 
+function fillJson5Table(sheetEmbedTag) {
+    let tomlUrl = sheetEmbedTag.getAttribute("src");
+    let headerStr = sheetEmbedTag.getAttribute("headers") || "";
+    $.ajax({
+        url: tomlUrl,
+        type: "GET",
+        dataType: "text",
+        mimeType: "text/plain",
+        success: function(data){
+            if (data.length > 0) {
+                console.debug(data);
+                var jsonData = json5.parse(data);
+                fillTableFromJsObject(jsonData, sheetEmbedTag, headerStr);
+            }
+        }
+    });
 }
 
 function fillTsvTable(sheetEmbedTag) {
@@ -126,6 +148,8 @@ function fillTable(sheetEmbedTag) {
         fillJsonTable(sheetEmbedTag);
     } else if (srcUrl.endsWith("tsv") || srcUrl.endsWith("csv")) {
         fillTsvTable(sheetEmbedTag);
+    } else if (srcUrl.endsWith("json5")) {
+        fillJson5Table(sheetEmbedTag);
     } else {
         console.error("Don't know how to deal with this file type: " + srcUrl)
     }
