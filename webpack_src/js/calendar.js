e47e437df8e126ad71c429a38e57577b0425a91d
vvasuki <vishvas.vasuki@gmail.com>
2020-11-17 10:58:41 +0530

    CU

---
+++ webpack_src/js/calendar.js
@@ -0,0 +1,18 @@
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+
+function fillIcsCalendar(embedTag) {
+    let icsUrl = embedTag.getAttribute("src");
+    $.getJSON(jsonUrl, function (data) {
+        if (data.length > 0) {
+        //    Await acceptance and merging of https://github.com/fullcalendar/fullcalendar/pull/5969  for full implementation.
+        }
+    });
+}
+
+export function fillCalendars() {
+    var sheetEmbedTags = document.querySelectorAll('.calendar');
+    sheetEmbedTags.forEach(function (embedTag) {
+        fillIcsCalendar(embedTag);
+    });
+}
