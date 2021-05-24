16c952bab11eda8f02d21e6c02f09ed1c34767c8
vvasuki <vishvas.vasuki@gmail.com>
2021-05-24 14:03:13 +0530

    CU

--- webpack_src/js/textToSpeech.js
+++ webpack_src/js/textToSpeech.js
@@ -1,58 +1,28 @@
 import * as utils from "./utils";
 import Sanscript from "@sanskrit-coders/sanscript";
 
-var suvaggaurvm=4789213;
-
-var suvak_svr0 = 106.67, suvak_svr1 = 120;
-var suvak_svritanudatth = suvak_svr0;
-var suvak_svritodatth = suvak_svr1;
-var suvak_udattpurvanudatth = 100;
-var suvak_adisvrh = 100;
-var udattadnudattsysvrith=true;
-var suvak_vegh=1.0;
-var prtikrm_=()=>{};
+var ekashrutiSvarah = 106.67, udaattaSvarah = 150;
+var svaritAnudaattah = ekashrutiSvarah;
+var svaritOdaattah = udaattaSvarah;
+var sannataraSvarah = 100;
+var aadiSvarah = ekashrutiSvarah;
+var udaattaadAnudaattasyaSvaritah = true;
+var speed = 1.0;
 
 
-var context = new AudioContext({sampleRate:48000,latencyHint:"playback"});
+
+var context = new AudioContext({sampleRate: 48000, latencyHint: "playback"});
+
+var node;
 
 export async function setup() {
-  var non_webpack_js_base = baseURL + "non_webpack_js/";
-  const audioWorkletUrl = new URL(non_webpack_js_base + "suvakworker.js");
-  console.log(audioWorkletUrl.href);
-  console.log('suvak2.js -> '+location.href);
-  function AudioWorklet(url) {
-    return url;
-  }
-  await context.audioWorklet.addModule(new AudioWorklet(audioWorkletUrl));
+    var non_webpack_js_base = baseURL + "non_webpack_js/";
+    const audioWorkletUrl = new URL(non_webpack_js_base + "suvakworker.js");
+    console.log(audioWorkletUrl.href);
+    console.log('suvak2.js -> ' + location.href);
+
+    await context.audioWorklet.addModule(audioWorkletUrl);
 }
-
-var suvak_reload;
-var suvagarbdih=false;
-var suvakw=false;
-
-
-var suvacnm=false;
-var suvakkrmh=0;
-
-function speak(vakym) {
-	let node = new AudioWorkletNode(context, 'suvak-processor');
-	node.port.onmessage = (event) => {
-		console.log('message')
-		console.log(event)
-  };
-  let sa=suvak_svr0;
-  let su=suvak_svr1;
-  let snn=suvak_svr0;
-  let adi=suvak_svr0;
-  if(suvak_svritanudatth>0)sa=suvak_svritanudatth;
-  if(suvak_adisvrh>0)adi=suvak_adisvrh;
-  if(suvak_svritodatth>0)su=suvak_svritodatth;
-  if(suvak_udattpurvanudatth>0)snn=suvak_udattpurvanudatth;
-  node.port.postMessage([vakym,
-  	Math.log(suvak_svr0),Math.log(suvak_svr1),Math.log(sa),Math.log(su),Math.log(snn),Math.log(adi),
-  	udattadnudattsysvrith,suvak_vegh]);
-}
-
 
 let ttsOn = false;
 
@@ -68,21 +38,31 @@ export function handleSpeakToggle() {
     }
 }
 
-
+export async function speak(text) {
+    node = new AudioWorkletNode(context, 'suvak-processor');
+    node.port.onmessage = (event) => {
+        console.log('Message from audio processor received')
+        console.log(event)
+    };
+    node.connect(context.destination);
+    console.log("AudioWorkletNode with suvak-processor created and connected to destination.", context.destination)
+    node.port.postMessage([text,
+        Math.log(ekashrutiSvarah), Math.log(udaattaSvarah), Math.log(svaritAnudaattah), Math.log(svaritOdaattah), Math.log(sannataraSvarah), Math.log(aadiSvarah),
+        udaattaadAnudaattasyaSvaritah, speed]);
+}
 
 export function speakAll() {
     ttsOn = true;
-    speak("ब्रह्मा रुद्रः कुमारो हरिवरुणयमा वह्निरिन्द्रः");
-    return;
-    // let transliterationSource = pageVars.unicodeScript || "devanagari";
-    // var textNodes = utils.textNodesUnder(document.getElementsByTagName("main")[0]);
-    // console.debug(textNodes);
-    // textNodes.forEach(function (textNode) {
-    //     if(textNode.parentNode.nodeName.toLowerCase() != "option") {
-    //         // console.debug(textNode.textContent, transliterationSource, transliterationTarget);
-    //         let transliteratedText = Sanscript.t(textNode.textContent, transliterationSource, "devanagari");
-    //         speak(transliteratedText);
-    //
-    //     }
-    // })
+    // speak("ब्रह्मा रुद्रः कुमारो हरिवरुणयमा वह्निरिन्द्रः");
+    // return;
+    let transliterationSource = pageVars.unicodeScript || "devanagari";
+    var textNodes = utils.textNodesUnder(document.getElementsByTagName("main")[0]);
+    console.debug(textNodes);
+    textNodes.forEach(function (textNode) {
+        if(textNode.parentNode.nodeName.toLowerCase() != "option") {
+            // console.debug(textNode.textContent, transliterationSource, transliterationTarget);
+            let transliteratedText = Sanscript.t(textNode.textContent, transliterationSource, "devanagari");
+            speak(transliteratedText);
+        }
+    })
 }
