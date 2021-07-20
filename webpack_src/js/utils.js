138a28b6a3637f99df5d05fb1e18961bfe728b86
vvasuki <vishvas.vasuki@gmail.com>
2021-07-20 09:52:58 +0530

    CU

--- webpack_src/js/utils.js
+++ webpack_src/js/utils.js
@@ -1,4 +1,12 @@
-
+export async function replaceAsync(str, regex, asyncFn) {
+    const promises = [];
+    str.replace(regex, (match, ...args) => {
+        const promise = asyncFn(match, ...args);
+        promises.push(promise);
+    });
+    const data = await Promise.all(promises);
+    return str.replace(regex, () => data.shift());
+}
 export function textNodesUnder(node){
     var all = [];
     for (node=node.firstChild;node;node=node.nextSibling){
