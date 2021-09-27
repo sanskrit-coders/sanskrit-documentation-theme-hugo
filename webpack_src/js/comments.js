1805e0e24edf995f8cdd1d2b9e3c08e0bc5ed06a
vvasuki <vishvas.vasuki@gmail.com>
2021-09-27 11:11:52 +0530

    CU

--- webpack_src/js/comments.js
+++ webpack_src/js/comments.js
@@ -5,7 +5,7 @@ export function setInlineComments(htmlIn) {
     let commentStyleDropdown = document.getElementsByName("commentStyleDropdown")[0];
     commentStyleDropdown.value = commentStyle;
     if (commentStyle === "hidden") {
-        console.debug(htmlIn);
+        // console.debug(htmlIn);
         let htmlOut = htmlIn;
         // Handle comment strings which span full line(s).
         htmlOut = htmlOut.replace(/(<br>|<p>)\+\+\+([\s\S]+?)\+\+\+(<br>)?/g, "$1<span class=\"inline_comment\" hidden>$2</span>");
