471f85eb8b49f04bf0e12e5a20406ec35f69da3e
Vishvas Vasuki विश्वासः <vishvas.vasuki@gmail.com>
2022-02-16 07:26:37 +0530

    CU

--- webpack_src/js/textToSpeech.js
+++ webpack_src/js/textToSpeech.js
@@ -11,17 +11,17 @@ var speed = 1.0;
 
 
 
-var context;
+var audioContext;
 
-var node;
+var audioWorkletNode;
 
 export async function setup() {
     var non_webpack_js_base = baseURL + "non_webpack_js/";
     const audioWorkletUrl = new URL(non_webpack_js_base + "suvakworker.js");
     console.log(audioWorkletUrl.href);
     console.log('suvak2.js -> ' + location.href);
-    context = new AudioContext({sampleRate: 48000, latencyHint: "playback"});
-    await context.audioWorklet.addModule(audioWorkletUrl);
+    audioContext = new AudioContext({sampleRate: 48000, latencyHint: "playback"});
+    await audioContext.audioWorklet.addModule(audioWorkletUrl);
 }
 
 let ttsOn = false;
@@ -39,32 +39,36 @@ export function handleSpeakToggle() {
 }
 
 export async function speak(text) {
-    await context.resume();
-    node = new AudioWorkletNode(context, 'suvak-processor');
-    node.port.onmessage = (event) => {
-        console.log('Message from audio processor received')
-        console.log(event)
+    await audioContext.resume();
+    audioWorkletNode = new AudioWorkletNode(audioContext, 'suvak-processor');
+    audioWorkletNode.port.onmessage = (event) => {
+        console.log('Message from audio processor received', event);
     };
-    node.connect(context.destination);
-    console.log("AudioWorkletNode with suvak-processor created and connected to destination.", context.destination)
-    node.port.postMessage([text,
+    audioWorkletNode.connect(audioContext.destination);
+    console.log("AudioWorkletNode with suvak-processor created and connected to destination.", audioContext.destination, text);
+    // text = "ब्रह्मा रुद्रः कुमारो हरिवरुणयमा वह्निरिन्द्रः";
+    audioWorkletNode.port.postMessage([text,
         Math.log(ekashrutiSvarah), Math.log(udaattaSvarah), Math.log(svaritAnudaattah), Math.log(svaritOdaattah), Math.log(sannataraSvarah), Math.log(aadiSvarah),
         udaattaadAnudaattasyaSvaritah, speed]);
 }
 
 export async function speakAll() {
     ttsOn = true;
+    await setup();
     // speak("ब्रह्मा रुद्रः कुमारो हरिवरुणयमा वह्निरिन्द्रः");
     // return;
-    await setup();
     let transliterationSource = pageVars.unicodeScript || "devanagari";
     var textNodes = utils.textNodesUnder(document.getElementsByTagName("main")[0]);
     console.debug(textNodes);
-    textNodes.forEach(function (textNode) {
+    let transliteratedText = "";
+      textNodes.forEach(function (textNode) {
         if(textNode.parentNode.nodeName.toLowerCase() != "option") {
             // console.debug(textNode.textContent, transliterationSource, transliterationTarget);
-            let transliteratedText = Sanscript.t(textNode.textContent, transliterationSource, "devanagari");
-            speak(transliteratedText);
+            let text = textNode.textContent.trim();
+            if (text != "") {
+                transliteratedText += " " + Sanscript.t(text, transliterationSource, "devanagari");
+            }
         }
     })
+    speak(transliteratedText);
 }
