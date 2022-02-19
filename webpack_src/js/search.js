992f4aa36a99d52ab978b1d832f5e95298eea211
Vishvas Vasuki विश्वासः <vishvas.vasuki@gmail.com>
2022-02-19 09:13:30 +0530

    CU

--- webpack_src/js/search.js
+++ webpack_src/js/search.js
@@ -1,29 +1,28 @@
-import 'webpack-jquery-ui';
-
 import * as dirTree from "./dirTree";
 import urljoin from 'url-join';
 
-function pageLoader() {
-  let pageSelected = $("#searchInput").val();
+export function pageLoader() {
+  let pageSelected = document.querySelector("#searchInput").value;
+  console.log("pageLoader for search input", pageSelected)
   if (pageSelected == "") {
     return;
   }
-  let url = pageSelected.split(";;")[1];
-  window.location = urljoin(baseURL, url);
+  window.location = urljoin(baseURL, pageSelected);
 }
 
 export function setupTitleSearch() {
-  // console.debug(sutraBasicsMap);
-  let autocompleteList = [];
   const urls = dirTree.getAllPaths(pageRelUrlTree);
+  let fragment = document.createDocumentFragment();
+  let pageDataList = document.getElementById("pageDataList")
   urls.forEach (url  => {
     let pageParams = dirTree.getPageParams(url);
     let title = pageParams["title"] || "";
     let autocompleteText = `${title};;${url}`;
-    autocompleteList.push(autocompleteText);
+    let option = document.createElement('option');
+    let text = document.createTextNode(autocompleteText);
+    option.appendChild(text);
+    option.value = url;
+    fragment.appendChild(option);
   });
-  $("#searchInput").autocomplete({
-    source: autocompleteList
-  });
-  $("#searchInput").change(pageLoader);
+  pageDataList.appendChild(fragment);
 }
